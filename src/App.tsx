a8cdceaa1a675c3b60b95ed163e686cf574cab12
ydnamjs <ydnamjs@udel.edu>
2022-09-09 02:28:54 -0400

    Added a new header, some cheese, a list about cheese, made the header objectively better, a button that probably works, and two red guys

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,12 +8,50 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <h1>New Header</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <p>I am Michael Snead OOooOOOooOo</p>
             <p>Hello World</p>
+            <p>Top 5 Cheese</p>
+            <img
+                src={require("./assets/images/cheese.jpg")}
+                alt="A picture of some cheese"
+            />
+            <ol>
+                <li>Cheddah</li>
+                <li>Blue</li>
+                <li>Mac and</li>
+                <li>Bleu</li>
+            </ol>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: 200,
+                                height: 300
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: 400,
+                                height: 200
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <div style={{ height: 200 }}></div>
         </div>
     );
 }
