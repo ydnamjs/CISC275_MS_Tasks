5a185b6811d5c5d3f0d9622f47a6720a8d2fee2b
ydnamjs <ydnamjs@udel.edu>
2022-09-20 14:51:30 -0400

    implemented the countShortWords function

--- src/arrays.ts
+++ src/arrays.ts
@@ -76,7 +76,7 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    return words.filter((word: string): boolean => word.length < 4).length;
 }
 
 /**
