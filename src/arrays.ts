06c034bf3265460503e2b144ab7a6021e35d2ffa
ydnamjs <ydnamjs@udel.edu>
2022-09-20 15:10:30 -0400

    implemented the makeMath function

--- src/arrays.ts
+++ src/arrays.ts
@@ -108,7 +108,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum = addends.reduce(
+        (currentTotal: number, num: number) => currentTotal + num,
+        0
+    );
+
+    return sum + "=" + addends.join("+");
 }
 
 /**
