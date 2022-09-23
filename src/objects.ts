1b1b6a4d929916cc9273a3e723064f5f38fb0e72
ydnamjs <ydnamjs@udel.edu>
2022-09-22 22:55:32 -0400

    implemented the publishQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -117,7 +117,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const publishedQuestion: Question = {
+        ...question,
+        published: !question.published
+    };
+    return publishedQuestion;
 }
 
 /**
