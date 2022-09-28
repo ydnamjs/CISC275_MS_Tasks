ba635c791d5f240777a1b35529ea3e5bb62428fb
ydnamjs <ydnamjs@udel.edu>
2022-09-28 17:58:41 -0400

    implemented the getNonEmptyQuestions function

--- src/nested.ts
+++ src/nested.ts
@@ -18,7 +18,15 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    const newQuestions = questions.filter(
+        (question: Question): boolean =>
+            !(
+                question.body === "" &&
+                question.expected == "" &&
+                question.options.length == 0
+            )
+    );
+    return newQuestions;
 }
 
 /***
