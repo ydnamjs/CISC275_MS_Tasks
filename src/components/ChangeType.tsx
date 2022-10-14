dccb1c84681599d624b56909d199dc753f46a61e
ydnamjs <ydnamjs@udel.edu>
2022-10-13 20:10:20 -0400

    implemented the ChangeType component

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,28 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function invertQuestionType(): void {
+        if (questionType === "multiple_choice_question") {
+            setQuestionType("short_answer_question");
+        } else {
+            setQuestionType("multiple_choice_question");
+        }
+    }
+
+    return (
+        <div>
+            {" "}
+            <Button onClick={invertQuestionType}>Change Type</Button>
+            {(questionType === "multiple_choice_question" && (
+                <div>Multiple Choice</div>
+            )) ||
+                (questionType === "short_answer_question" && (
+                    <div>Short Answer</div>
+                ))}
+        </div>
+    );
 }
