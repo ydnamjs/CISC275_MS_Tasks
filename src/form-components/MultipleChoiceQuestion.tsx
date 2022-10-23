552008c74e916140588c653614f973f5a6789602
ydnamjs <ydnamjs@udel.edu>
2022-10-22 23:15:54 -0400

    implemented the MultipleChoiceQuestion component

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            {answer === expectedAnswer && <p>✔️</p>}
+            {answer !== expectedAnswer && <p>❌</p>}
+            <Form.Group>
+                <Form.Label>What is the Answer?</Form.Label>
+                <Form.Select value={answer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
         </div>
     );
 }
